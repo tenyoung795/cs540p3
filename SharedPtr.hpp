6076866d42d7557f0eeba5c96a3d3d70e7a6dcb1
Ten-Young Guh <tenyoung795@gmail.com>
2016-04-21 17:38:04 -0400

    Pass functionalty tests in basic tests 1

--- SharedPtr.hpp
+++ SharedPtr.hpp
@@ -6,27 +6,44 @@
 #include <stdexcept>
 
 namespace cs540 {
+class _DynamicTag {
+    template <typename>
+    friend class SharedPtr;
+
+    _DynamicTag() = default;
+};
 
 template <typename T>
 class SharedPtr {
+    template <typename>
+    friend class SharedPtr;
+
+    T *_ptr;
+
+    template <typename U>
+    explicit SharedPtr(_DynamicTag, const SharedPtr<U> &sp) :
+        _ptr{dynamic_cast<T *>(sp._ptr)} {}
+
 public:
-    constexpr SharedPtr() = default;
+    constexpr SharedPtr() : _ptr{} {}
 
     template <typename U>
-    explicit SharedPtr(U *) {}
+    explicit SharedPtr(U *ptr) : _ptr{ptr} {}
 
     template <typename U>
-    SharedPtr(const SharedPtr<U> &) {}
+    SharedPtr(const SharedPtr<U> &sp) : _ptr{static_cast<T *>(sp._ptr)} {}
 
     void reset() {
+        _ptr = nullptr;
     }
 
     template <typename U>
-    void reset(U *) {
+    void reset(U *ptr) {
+        _ptr = ptr;
     }
 
     constexpr T *get() const {
-        throw std::runtime_error {__PRETTY_FUNCTION__};
+        return _ptr;
     }
 
     T &operator*() const {
@@ -40,6 +57,9 @@ public:
     constexpr explicit operator bool() const {
         return get();
     }
+
+    template <typename U, typename V>
+    friend SharedPtr<U> dynamic_pointer_cast(const SharedPtr<V> &);
 };
 
 template <typename T1, typename T2>
@@ -79,9 +99,8 @@ static SharedPtr<T> static_pointer_cast(const SharedPtr<U> &sp) {
 
 template <typename T, typename U>
 static SharedPtr<T> dynamic_pointer_cast(const SharedPtr<U> &sp) {
-    return SharedPtr<T> {sp};
+    return SharedPtr<T> {_DynamicTag{}, sp};
 }
-
 }
 
 #endif // CS540_SHARED_PTR_HPP
